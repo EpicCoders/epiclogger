9d4bfa3f5315f21c4944f32f449bdb4e0560c4ce
Razvan Ciocanel <chocksy@gmail.com>
2015-07-20 19:01:03 +0300

    removed js routes init and updated the optiosn in the routes.js file

--- config/initializers/jsroutes.rb
+++
@@ -1,6 +0,0 @@
-JsRoutes.setup do |config|
-  config.default_url_options  = {:host => ENV['MAILER_HOST']}
-  config.url_links            = true
-  # config.prefix               = "http://#{ENV['MAILER_HOST']}"
-  config.exclude              = [/admin/]
-end
