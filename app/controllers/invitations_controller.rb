7c254dfe19aa57af740bf197de003ace85ff759f
panioglo <panioglo.srj@gmail.com>
2015-08-31 17:40:39 +0300

    removed binding.pry

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,7 +2,6 @@ class InvitationsController < ApplicationController
 	def new
 	end
 	def show
-  	binding.pry
     if !Member.find_by_email(params[:email]).present?
       signup_params = '?website_id=' + params[:id] + '&email=' + params[:email] + '&token=' + params[:token]
       redirect_to signup_url() + signup_params
