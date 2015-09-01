ae7997ca988ce1d551d3419777758ed43958aab8
aurelianb <b.relu93@gmail.com>
2015-09-01 21:17:22 +0300

    Fixed the failing spec

--- spec/api/auth_spec.rb
+++ spec/api/auth_spec.rb
@@ -23,7 +23,7 @@ describe "Auth API" do
     expect(response).to be_success
 
     json = JSON.parse(response.body)
-    expect(json).to eq({'errors'=> []}, 'page' => 1, 'pages' => 0)
+    expect(json).to eq({'errors'=> [], 'page' => 1, 'pages' => 0})
   end
 
 end
