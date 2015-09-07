ad9fc9128653cb6774c2a55c9f55d71e3ea5118e
Alin Spiridon <spiridon.alin@gmail.com>
2015-09-07 07:26:31 +0300

    - added status for grouped issues

--- app/models/grouped_issue.rb
+++ app/models/grouped_issue.rb
@@ -2,5 +2,6 @@ class GroupedIssue < ActiveRecord::Base
 
   enumerize :level, in: {:debug => 1, :info => 2, :warning => 3, :error => 4, :fatal => 5}, default: :error
   enumerize :logger, in: {:javascript => 1, :php => 2}, default: :javascript
+  enumerize :status, in: {:unresolved => 1, :resolved => 2, :muted => 3}
 
 end
