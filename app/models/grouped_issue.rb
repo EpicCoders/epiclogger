a4005f71c2a036603066f2eac1616e48ba97003d
Razvan Ciocanel <chocksy@gmail.com>
2015-09-09 11:25:32 +0300

    fixed the grouped_issue spec

--- app/models/grouped_issue.rb
+++ app/models/grouped_issue.rb
@@ -1,7 +1,6 @@
 class GroupedIssue < ActiveRecord::Base
   extend Enumerize
   belongs_to :website
-  has_many :websites
   enumerize :level, in: {:debug => 1, :error => 2, :fatal => 3, :info => 4, :warning => 5}, default: :error
   enumerize :issue_logger, in: {:javascript => 1, :php => 2}, default: :javascript
   enumerize :status, in: {:muted => 1, :resolved => 2, :unresolved => 3}
--- spec/models/grouped_issue_spec.rb
+++ spec/models/grouped_issue_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe GroupedIssue do
-  it { is_expected.to enumerize(:level).in(debug: 1, error: 2, fatal: 3, info: 4, warning: 5).with_default(:error) }
-  it { is_expected.to enumerize(:issue_logger).in(javascript: 1, php: 2).with_default(:javascript) }
-  it { is_expected.to enumerize(:status).in(muted: 1, resolved: 2, unresolved: 3) }
   let(:grouped_issue) { build(:grouped_issue) }
+  it { is_expected.to enumerize(:level).in(:debug, :error, :fatal, :info, :warning).with_default(:error) }
+  it { is_expected.to enumerize(:issue_logger).in(:javascript, :php).with_default(:javascript) }
+  it { is_expected.to enumerize(:status).in(:muted, :resolved, :unresolved) }
 
   it "has a valid factory" do
-    expect(build(:grouped_issue)).to be_valid
+    expect(grouped_issue).to be_valid
   end
 end
