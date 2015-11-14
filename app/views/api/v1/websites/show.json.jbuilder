af51a2c6df1aac5fdd9c8723aeddacb9ac20c69f
Razvan Ciocanel <chocksy@gmail.com>
2015-11-14 18:36:08 +0200

    moved away from app_id in the code

--- app/views/api/v1/websites/show.json.jbuilder
+++ app/views/api/v1/websites/show.json.jbuilder
@@ -1,4 +1,4 @@
-json.(@website, :id, :app_id, :app_key, :domain, :title, :new_event, :frequent_event, :daily, :realtime)
+json.(@website, :id, :app_secret, :app_key, :domain, :title, :new_event, :frequent_event, :daily, :realtime)
 json.errors @website.grouped_issues.count
 json.subscribers @website.subscribers.count
 json.members @website.members.count
--- spec/controllers/api/v1/websites_controller_spec.rb
+++ spec/controllers/api/v1/websites_controller_spec.rb
@@ -35,7 +35,7 @@ describe Api::V1::WebsitesController, :type => :controller do
               id: website.id,
               title: website.title,
               domain: website.domain,
-              app_id: website.app_id,
+              app_secret: website.app_secret,
               app_key: website.app_key,
               errors: website.grouped_issues.count,
               subscribers: website.subscribers.count,
@@ -136,8 +136,8 @@ describe Api::V1::WebsitesController, :type => :controller do
         expect(response.body).to eq(
           {
             id: website.id,
-            app_id: website.app_id,
             app_key: website.app_key,
+            app_secret: website.app_secret,
             domain: website.domain,
             title: website.title,
             new_event: website.new_event,
