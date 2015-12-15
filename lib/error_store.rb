81ca77a6d8c7e4a3a80b5b3dc2efe8c902631163
Razvan Ciocanel <chocksy@gmail.com>
2015-12-15 14:09:29 +0200

    change the client_reserved_attrs to use symbols as we are using symbolize keys now

--- lib/error_store.rb
+++ lib/error_store.rb
@@ -20,25 +20,25 @@ module ErrorStore
   }
 
   CLIENT_RESERVED_ATTRS = [
-    'project',
-    'errors',
-    'event_id',
-    'message',
-    'checksum',
-    'culprit',
-    'fingerprint',
-    'level',
-    'time_spent',
-    'logger',
-    'server_name',
-    'site',
-    'timestamp',
-    'extra',
-    'modules',
-    'tags',
-    'platform',
-    'release',
-    'environment'
+    :project,
+    :errors,
+    :event_id,
+    :message,
+    :checksum,
+    :culprit,
+    :fingerprint,
+    :level,
+    :time_spent,
+    :logger,
+    :server_name,
+    :site,
+    :timestamp,
+    :extra,
+    :modules,
+    :tags,
+    :platform,
+    :release,
+    :environment
   ]
   VALID_PLATFORMS = [
     'as3',
@@ -57,12 +57,24 @@ module ErrorStore
     'ruby',
   ]
 
+  LOG_LEVELS = {
+      10 => 'debug',
+      20 => 'info',
+      30 => 'warning',
+      40 => 'error',
+      50 => 'fatal',
+  }
+
+  DEFAULT_LOG_LEVEL     = 'error'
+  DEFAULT_LOGGER_NAME   = ''
   MAX_STACKTRACE_FRAMES = 50
   MAX_HTTP_BODY_SIZE    = 4096 * 4  # 16kb
   MAX_EXCEPTIONS        = 25
   MAX_HASH_ITEMS        = 50
   MAX_STACKTRACE_FRAMES = 50
   MAX_VARIABLE_SIZE     = 512
+  MAX_CULPRIT_LENGTH    = 200
+  MAX_MESSAGE_LENGTH    = 1024 * 8
   HTTP_METHODS          = ['GET', 'POST', 'PUT', 'OPTIONS', 'HEAD', 'DELETE', 'TRACE', 'CONNECT', 'PATCH']
 
   class StoreError < StandardError
