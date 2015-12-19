86374e90c1686188df7b3189f05602993440e73a
Razvan Ciocanel <chocksy@gmail.com>
2015-12-19 17:16:45 +0200

    add mailer previews to application.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.preview_path = "#{Rails.root}/spec/mailer_previews"
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = {:host => ENV['MAILER_HOST']}
