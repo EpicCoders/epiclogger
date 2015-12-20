dd0b9237e133f89ecf203947c715f501f2032ac7
Razvan Ciocanel <chocksy@gmail.com>
2015-12-20 18:28:32 +0200

    commented out the email notification for now of grouped_issue

--- app/models/grouped_issue.rb
+++ app/models/grouped_issue.rb
@@ -5,15 +5,19 @@ class GroupedIssue < ActiveRecord::Base
   has_many :issues, foreign_key: 'group_id', dependent: :destroy
   enumerize :level, in: {:debug => 1, :error => 2, :fatal => 3, :info => 4, :warning => 5}, default: :error
   enumerize :issue_logger, in: {:javascript => 1, :php => 2}, default: :javascript
-  enumerize :status, in: {:muted => 1, :resolved => 2, :unresolved => 3}
+  enumerize :status, in: {:muted => 1, :resolved => 2, :unresolved => 3}, default: :unresolved
   after_create :group_created
 
   def group_created
-    UserMailer.event_occurred(self.website_id, self.id).deliver_now
+    # UserMailer.event_occurred(self.website_id, self.id).deliver_now
   end
 
   def error
     ErrorStore.find(self)
+  end
+
+  def is_resolved?
+    status.resolved?
   end
 
 end
