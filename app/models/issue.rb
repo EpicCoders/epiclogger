77338233410c8d93248882142ee84b56e87dc9bc
Razvan Ciocanel <chocksy@gmail.com>
2015-12-20 18:28:48 +0200

    add error and get_interfaces method for issue

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,8 +5,16 @@ class Issue < ActiveRecord::Base
   belongs_to :website
   belongs_to :group, class_name: 'GroupedIssue', foreign_key: 'group_id'
   accepts_nested_attributes_for :messages
-  enumerize :platform, in: {:javascript => 1, :php => 2}, default: :javascript
+  # enumerize :platform, in: {:javascript => 1, :php => 2}, default: :javascript
 
   validates :message, :presence => true, length: {minimum: 10}
+
+  def error
+    ErrorStore.find(self)
+  end
+
+  def get_interfaces
+    error._get_interfaces
+  end
 
 end
