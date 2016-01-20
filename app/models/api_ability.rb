bbcbcde432c2568d2a3995261d1cfd1dc79f04e4
Razvan Ciocanel <chocksy@gmail.com>
2016-01-20 11:07:09 +0200

    fix api_ability to use required conditions

--- app/models/api_ability.rb
+++ app/models/api_ability.rb
@@ -3,13 +3,31 @@ class ApiAbility
 
   def initialize(member)
     if member
-      can :manage, GroupedIssue
-      can :manage, Issue
-      can :manage, Message
-      can :manage, Notification
-      can :manage, Subscriber
-      can :manage, Website
-      can :manage, WebsiteMember
+      can :manage, Member, id: member.id
+      can :manage, GroupedIssue do |group|
+        member.is_owner_of?(group.website)
+      end
+      can :manage, Issue do |issue|
+        member.is_owner_of?(issue.group.website)
+      end
+      can :manage, Message do |message|
+        member.is_owner_of?(message.issue.group.website)
+      end
+      can :manage, Notification do |notification|
+        member.is_owner_of?(notification.website)
+      end
+      can :manage, Subscriber do |subscriber|
+        member.is_owner_of?(subscriber.website)
+      end
+      can :manage, Website do |website|
+        member.is_owner_of?(website)
+      end
+      can :manage, WebsiteMember do |website_member|
+        member.is_owner_of?(website_member.website)
+      end
+      can :read, WebsiteMember do |website_member|
+        website_member.website.members.include?(member)
+      end
     end
   end
 end
