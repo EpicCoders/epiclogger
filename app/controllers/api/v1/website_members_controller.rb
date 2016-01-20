2b9a3adeace6bbba0e948ccb027581a74381d66e
Razvan Ciocanel <chocksy@gmail.com>
2016-01-20 10:48:38 +0200

    removed redundant find from destroy

--- app/controllers/api/v1/website_members_controller.rb
+++ app/controllers/api/v1/website_members_controller.rb
@@ -5,8 +5,7 @@ class Api::V1::WebsiteMembersController < Api::V1::ApiController
   end
 
   def destroy
-    @website_member = WebsiteMember.find(params[:id])
-    @website_member.destroy()
+    @website_member.destroy
     unless @website_member.errors.full_messages.blank?
       _not_allowed!(@website_member.errors.full_messages.first)
     end
