c55f3f3478448950c31214cba2d30488418a90d4
Razvan Ciocanel <chocksy@gmail.com>
2016-01-29 13:56:46 +0200

    add a check_fields method before save that creates timestamps depending on values

--- app/models/grouped_issue.rb
+++ app/models/grouped_issue.rb
@@ -3,10 +3,12 @@ class GroupedIssue < ActiveRecord::Base
   belongs_to :website
   has_many :subscribers, through: :issues, foreign_key: 'group_id'
   has_many :issues, foreign_key: 'group_id', dependent: :destroy
-  enumerize :level, in: {:debug => 1, :error => 2, :fatal => 3, :info => 4, :warning => 5}, default: :error
-  enumerize :issue_logger, in: {:javascript => 1, :php => 2}, default: :javascript
-  enumerize :status, in: {:muted => 1, :resolved => 2, :unresolved => 3}, default: :unresolved
+  enumerize :level, in: { debug: 1, error: 2, fatal: 3, info: 4, warning: 5 }, default: :error
+  enumerize :issue_logger, in: { javascript: 1, php: 2 }, default: :javascript
+  enumerize :status, in: { muted: 1, resolved: 2, unresolved: 3 }, default: :unresolved
   after_create :group_created
+
+  before_save :check_fields
 
   def group_created
     # UserMailer.event_occurred(self.website_id, self.id).deliver_now
@@ -20,4 +22,14 @@ class GroupedIssue < ActiveRecord::Base
     status.resolved?
   end
 
+  private
+
+  def check_fields
+    self.resolved_at = Time.now.utc if status_changed? && status.resolved?
+    self.last_seen = Time.now.utc if last_seen.blank?
+    self.first_seen = last_seen if first_seen.blank?
+    self.active_at = first_seen if active_at.blank?
+    # We limit what we store for the message body
+    self.message = message.truncate(255, separator: '') unless message.blank?
+  end
 end
