45d929b25f2ab491dbf7a5034b4aae6d8874164a
Razvan Ciocanel <chocksy@gmail.com>
2016-02-11 14:22:51 +0200

    identation fix for subscriber_spec

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -4,20 +4,20 @@ describe Subscriber do
   let(:website) { create(:website) }
   let(:subscriber) { build(:subscriber, website: website) }
 
-  it "has a valid factory" do
+  it 'has a valid factory' do
     expect(build(:subscriber)).to be_valid
   end
 
-  describe "ActiveModel validations" do
-    it "is invalid without an email" do
+  describe 'ActiveModel validations' do
+    it 'is invalid without an email' do
       expect(subscriber).to validate_presence_of :email
     end
 
-    it "is invalid without a website " do
+    it 'is invalid without a website ' do
       expect(subscriber).to validate_presence_of :website
     end
 
-    it "is invalid with duplication email" do
+    it 'is invalid with duplication email' do
       newsub = create(:subscriber, website: website)
       expect {
         create(:subscriber, email: newsub.email, website: website)
@@ -25,16 +25,13 @@ describe Subscriber do
     end
   end
 
-   describe "ActiveRecord associations" do
-
-     it "belongs_to a website" do
+  describe 'ActiveRecord associations' do
+    it 'belongs_to a website' do
       expect(subscriber).to belong_to(:website)
-     end
+    end
 
-    it "has many errors" do
+    it 'has many errors' do
       expect(subscriber).to have_many(:issues)
-     end
-
-   end
-
+    end
+  end
 end
