c59def8a3af62726364701af379a8fd92148b6d6
Razvan Ciocanel <chocksy@gmail.com>
2016-03-03 10:29:14 +0200

    fix the fontmizz issue with fonts-url

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,12 @@
 # Be sure to restart your server when you modify this file.
+Rails.application.configure do
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
-
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-# Rails.application.config.assets.paths << Rails.root.join("app", "assets", "pages")
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( pages/*.* )
-Rails.application.config.assets.precompile += %w( fontmizz/*.* )
+  # Add additional assets to the asset load path
+  # config.assets.paths << Emoji.images_path
+  # config.assets.paths << Rails.root.join("app", "assets", "pages")
+  # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  config.assets.precompile += %w( pages/*.* fontmizz/*.*)
+end
