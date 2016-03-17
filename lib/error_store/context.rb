995b6eda8f5fedb8fc9d1d1047de0247f5ff3b95
Razvan Ciocanel <chocksy@gmail.com>
2016-03-17 14:11:49 +0200

    complete context testing

--- lib/error_store/context.rb
+++ lib/error_store/context.rb
@@ -4,11 +4,8 @@ module ErrorStore
     def initialize(error)
       @error        = error
       @agent        = @error.request.headers['HTTP_USER_AGENT']
-      # @version      = version
       @website_id   = @error._params['id']
-      # @website      = website
       @ip_address   = @error.request.headers['REMOTE_ADDR']
     end
-    # Context = Struct.new(:agent, :version, :website_id, :website, :ip_address)
   end
 end
--- spec/lib/error_store/context_spec.rb
+++ spec/lib/error_store/context_spec.rb
@@ -2,32 +2,39 @@ require 'rails_helper'
 
 RSpec.describe ErrorStore::Context do
   let(:website) { create :website }
-  let(:group) { create :grouped_issue, website: website }
-  let(:subscriber) { create :subscriber, website: website }
-  let!(:issue_error) { create :issue, subscriber: subscriber, group: group, event_id: '8af060b2986f5914764d49b7f39b036c' }
-  let(:error) { ErrorStore::Error.new(request: ActionDispatch::Request.new('REQUEST_METHOD' => 'POST','HTTP_USER_AGENT'=>'Faraday v0.9.2','REMOTE_ADDR'=>'127.0.0.1','HTTP_X_SENTRY_AUTH' => "Sentry sentry_version='5', sentry_client='raven-ruby/0.15.2',sentry_timestamp=1455616740, sentry_key=#{website.app_key}, sentry_secret=#{website.app_secret}",'HTTP_ACCEPT_ENCOaDING' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3','rack.input' => StringIO.new(Base64.strict_encode64(Zlib::Deflate.deflate(issue_error.data))))) }
 
-  before { error.request.parameters.merge!("format"=>:json, "controller"=>"api/v1/store", "action"=>"create", "id"=>"1") }
+  let(:post_request) { post_error_request(web_response_factory('ruby_exception'), website) }
+  let(:get_request) { get_error_request(web_response_factory('js_exception'), website) }
+  let(:post_error) { ErrorStore::Error.new(request: post_request) }
+  let(:get_error) { ErrorStore::Error.new(request: get_request) }
+  let(:post_context) { ErrorStore::Context.new(post_error) }
+  let(:get_context) { ErrorStore::Context.new(get_error) }
 
   describe 'initialize' do
     it 'assigns error' do
-      expect( ErrorStore::Context.new(error).instance_variable_get(:@error) ).to eq(error)
+      expect( get_context.instance_variable_get(:@error) ).to eq(get_error)
+      expect( post_context.instance_variable_get(:@error) ).to eq(post_error)
     end
     it 'assigns agent' do
-      expect( ErrorStore::Context.new(error).instance_variable_get(:@agent) ).to eq(error.request.headers['HTTP_USER_AGENT'])
+      expect( get_context.instance_variable_get(:@agent) ).to eq(get_error.request.headers['HTTP_USER_AGENT'])
+      expect( post_context.instance_variable_get(:@agent) ).to eq(post_error.request.headers['HTTP_USER_AGENT'])
     end
     it 'assigns website_id' do
-      expect( ErrorStore::Context.new(error).instance_variable_get(:@website_id) ).to eq(error.request.parameters['id'])
+      expect( get_context.instance_variable_get(:@website_id) ).to eq(website.id.to_s)
+      expect( post_context.instance_variable_get(:@website_id) ).to be_nil
     end
     it 'assigns ip_address' do
-      expect( ErrorStore::Context.new(error).instance_variable_get(:@ip_address) ).to eq(error.request.headers['REMOTE_ADDR'])
+      expect( get_context.instance_variable_get(:@ip_address) ).to eq(get_error.request.headers['REMOTE_ADDR'])
+      expect( post_context.instance_variable_get(:@ip_address) ).to eq(post_error.request.headers['REMOTE_ADDR'])
     end
   end
 
   it 'responds to website' do
-    expect( ErrorStore::Context.new(error).website ).to be_nil
+    expect( get_context ).to respond_to(:website)
+    expect( post_context ).to respond_to(:website)
   end
   it 'responds to version' do
-    expect( ErrorStore::Context.new(error).version ).to be_nil
+    expect( get_context ).to respond_to(:version)
+    expect( post_context ).to respond_to(:version)
   end
 end
