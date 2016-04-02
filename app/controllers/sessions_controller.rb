b65d7763003b754b1d102cac3cccd1d393d1a704
Razvan Ciocanel <chocksy@gmail.com>
2016-04-02 10:38:37 +0300

    redirect if already logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,14 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate! #, only: [:create]
 
   def new
+    after_login_redirect if logged_in?
     @user = User.new
   end
 
   def create
     user = authenticate!(:password)
     set_website(user.default_website)
-    if current_website
-      redirect_to errors_url, notice: "Logged in"
-    else
-      redirect_to new_website_url, notice: 'Logged in'
-    end
+    after_login_redirect
   end
 
   def destroy
@@ -23,5 +20,15 @@ class SessionsController < ApplicationController
 
   def unauthorized
     redirect_to login_url
+  end
+
+  protected
+
+  def after_login_redirect
+    if current_website
+      redirect_to errors_url, notice: "Logged in"
+    else
+      redirect_to new_website_url, notice: 'Logged in'
+    end
   end
 end
