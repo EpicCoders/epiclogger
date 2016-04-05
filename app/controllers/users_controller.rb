1eed80b3ab624d984a517b89c6c2d7fbac77f569
Alin <spiridon.alin@gmail.com>
2016-04-05 21:53:43 +0300

    - got the signup working again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,24 @@ class UsersController < ApplicationController
   def index; end
 
   def new
+    redirect_to websites_url if logged_in?
+
     @user = User.new
     gon.token = params[:id]
     gon.website_id = params[:website_id]
   end
 
   def create
-    @user = User.new(user_params)
-    @user.save
+    user = User.new(user_params)
+    user.save
+
+    authenticate!(:password)
+    redirect_to new_website_url
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params[:uid] = SecureRandom.hex(10)
+    params[:provider] = 'email'
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :provider, :uid)
   end
 end
