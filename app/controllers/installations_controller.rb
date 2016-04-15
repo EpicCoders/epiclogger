6ade4a47d279cbbc8d053bfa0805f969e28f79ad
panioglo <panioglo.srj@gmail.com>
2016-04-15 11:48:41 +0300

    more changes for installations index

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -1,7 +1,13 @@
 class InstallationsController < ApplicationController
   def index
     @website = current_website
-    @main_tab = params[:main_tab] || "details"
+    session[:main_tab] = params[:main_tab] if params[:main_tab].present?
+    @main_tab = session[:main_tab] || "details"
+
+    session[:configuration_tab] = params[:configuration_tab] if params[:configuration_tab].present?
+    @configuration_tab = session[:configuration_tab] || "all_platforms"
+    @platform_tab = params[:platform_tab] || @configuration_tab
+
     @details_tab = params[:details_tab] || "settings"
   end
 end
