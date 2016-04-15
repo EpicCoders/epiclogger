ef9fd0968e5bac70db9659eba5271ba19a96a5e3
panioglo <panioglo.srj@gmail.com>
2016-04-15 12:56:09 +0300

    simplified controller index method

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -1,13 +1,15 @@
 class InstallationsController < ApplicationController
   def index
     @website = current_website
-    session[:main_tab] = params[:main_tab] if params[:main_tab].present?
-    @main_tab = session[:main_tab] || "details"
+    @main_tab = assign_variables(:main_tab, "details")
 
-    session[:configuration_tab] = params[:configuration_tab] if params[:configuration_tab].present?
-    @configuration_tab = session[:configuration_tab] || "all_platforms"
+    @configuration_tab = assign_variables(:configuration_tab, "all_platforms")
     @platform_tab = params[:platform_tab] || @configuration_tab
+    @details_tab = assign_variables(:details_tab, "settings")
+  end
 
-    @details_tab = params[:details_tab] || "settings"
+  def assign_variables(tab, default)
+    session[tab] = params[tab] if params[tab].present?
+    return session[tab] || default
   end
 end
