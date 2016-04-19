5362cde0e8658eb6946abb253b431b210a65f05f
panioglo <panioglo.srj@gmail.com>
2016-04-19 09:42:51 +0300

    switch betwen tabs using params

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -1,17 +1,12 @@
 class InstallationsController < ApplicationController
   def index
     @website = current_website
-    @main_tab = assign_variables(:main_tab, "details")
+    @main_tab = params[:main_tab] || "details"
 
-    @details_tab = assign_variables(:details_tab, "settings")
-    @configuration_tab = assign_variables(:configuration_tab, "all_platforms")
+    @details_tab = params[:details_tab] || "settings"
+    @configuration_tab = params[:configuration_tab] || "all_platforms"
     @platform_tab = params[:platform_tab] || @configuration_tab
 
     @options = ["Javascript", "Python", "Django", "Flask", "Tornado", "Php", "Ruby", "Rails 3", "Rails 4", "Sinatra", "Sidekiq", "Node js", "Express", "Connect", "Java", "Java util logging", "Log4j", "Log4j2", "Logback", "Ios"]
-  end
-
-  def assign_variables(tab, default)
-    session[tab] = params[tab] if params[tab].present?
-    return session[tab] || default
   end
 end
