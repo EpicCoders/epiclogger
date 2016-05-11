061c913a5f4f59a5a6f0134e28e1dbbcca78d9ba
panioglo <panioglo.srj@gmail.com>
2016-05-11 16:00:59 +0300

    small fix

--- app/controllers/api/v1/release_controller.rb
+++ app/controllers/api/v1/release_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::ReleaseController < Api::V1::ApiController
     @website = Website.find(params[:id])
     @last_release = @website.releases.last
     unless @last_release.version == params[:head_long]
-      @last_release.grouped_issues.update_all(:status => 2)
-      Release.find_or_create_by(version: params[:head_long]) do |release|
+      @current_release = Release.find_or_create_by(version: params[:head_long]) do |release|
         release.data = params
         release.website_id = params[:id]
       end
+      @last_release.grouped_issues.update_all(:status => 2, :release_id => @current_release )
     end
   end
 end
