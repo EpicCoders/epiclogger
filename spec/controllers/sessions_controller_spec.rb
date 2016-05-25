e3972c17a94da76d4a6fed034f6c88eaaebaaf78
panioglo <panioglo.srj@gmail.com>
2016-05-25 09:36:24 +0300

    tests for sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
+  let(:user) { create :user }
+  let(:website) { create :website }
+  let!(:website_member) { create :website_member, website: website, user: user }
 
   describe "GET new" do
-    it "returns http success" do
+    it 'should user layout' do
       get :new
-      expect(response).to be_success
+      expect(response).to render_template(:layout => 'landing')
+    end
+
+    it 'returns http success' do
+      get :new
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns User.new' do
+      get :new
+      expect(assigns(:user)).to be_an_instance_of(User)
+    end
+
+    it 'should redirect to create' do
+      expect( get_with user, :new ).to redirect_to(website_wizard_path(:create))
     end
   end
 
+  describe "POST #create" do
+    let(:params) {{ user: { email: user.email, password: user.password } }}
+
+    it 'should auth user' do
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(:current_user).and_return(user)
+
+      post :create, params
+    end
+
+    it 'should set current_website' do
+      post :create, params
+      expect(session[:epiclogger_website_id]).to eq(website.id)
+    end
+
+    it 'should redirect to create' do
+      user2 = create :user, email: "user@email.com"
+      expect( post :create, user: { email: user2.email, password: user2.password } ).to redirect_to(website_wizard_path(:create))
+    end
+
+    it 'should redirect to errors' do
+      expect( post :create, user: { email: user.email, password: user.password } ).to redirect_to(errors_path)
+    end
+  end
 end
