44816ad95450e37b82399dc14158f1f94b3ded83
Razvan Ciocanel <chocksy@gmail.com>
2016-06-09 12:15:00 +0300

    store the cache for 1 hour not 1 minute

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,2 +1,2 @@
 redis_url = ENV['REDISCLOUD_URL'] || ENV['REDIS_URL'] || 'redis://127.0.0.1:6379/0/pxls'
-Epiclogger::Application.config.cache_store = :redis_store, redis_url, { expires_in: 1.minute }
+Epiclogger::Application.config.cache_store = :redis_store, redis_url, { expires_in: 1.hour }
