9575548af536efb9bcdd2811cc6c6b3311804dec
Razvan Ciocanel <chocksy@gmail.com>
2016-06-21 11:11:23 +0300

    fix the invites factory

--- spec/factories/invite.rb
+++ spec/factories/invite.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :invite do
     association :website
-    invited_by_id  1
+    association :invited_by_id, factory: :user
     sequence(:email) { |n| "person#{n+1}@example.com" }
     token "sJCyJOe72EgbDIHA-x82Ow"
   end
