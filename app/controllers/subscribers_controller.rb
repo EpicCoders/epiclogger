2085d5157f19bd3bc68226513aaeebe713f13c06
panioglo <panioglo.srj@gmail.com>
2016-06-22 09:30:51 +0300

    updates for subscribers view

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,5 +1,14 @@
 class SubscribersController < ApplicationController
+	load_and_authorize_resource
   def index
     @subscribers = current_website.subscribers
+  end
+
+  def destroy
+    binding.pry
+    @subscriber.destroy
+    respond_to do |format|
+      format.js { render inline: 'location.reload();' }
+    end
   end
 end
