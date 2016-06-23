c040f1b88990c367371b66670edb8ac962febced
Razvan Ciocanel <chocksy@gmail.com>
2016-06-23 11:56:19 +0300

    adjust ability_spec

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -5,29 +5,21 @@ RSpec.describe Ability do
     describe 'abilities' do
       let(:admin){ create :user, role: 1 }
       let(:user) { create :user }
+      let(:owner) { create :user }
       let(:website) { create :website }
       let!(:user_website_member_with_user) { create :website_member, website: website, user: user }
-      let!(:owner_website_member_with_user) { create :website_member, website: website, user: user, role: 1 }
+      let!(:owner_website_member_with_user) { create :website_member, website: website, user: owner, role: 1 }
       let!(:user_website_member_with_admin) { create :website_member, website: website, user: admin }
-      let!(:owner_website_member_with_admin) { create :website_member, website: website, user: admin, role: 1 }
       let(:group) { create :grouped_issue, website: website }
       let(:subscriber) { create :subscriber, website: website }
       let(:issue) { create :issue, subscriber: subscriber, group: group }
       let(:message) { create :message, issue: issue }
 
-      context 'admin' do
-        let(:ability){ Ability.new(admin) }
+      context 'as owner of org' do
+        subject(:ability){ Ability.new(owner) }
 
-        it 'can manage' do
-          assert ability.can?(:manage, admin)
-          assert ability.can?(:manage, website)
-          assert ability.can?(:manage, user_website_member_with_admin)
-          assert ability.can?(:manage, owner_website_member_with_admin)
-          assert ability.can?(:manage, group)
-          assert ability.can?(:manage, subscriber)
-          assert ability.can?(:manage, issue)
-          assert ability.can?(:manage, message)
-        end
+        it { is_expected.to be_able_to(:manage, owner) }
+        it { is_expected.not_to be_able_to(:manage, user) }
       end
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+require 'cancan/matchers'
 require 'sidekiq/testing'
 require 'capybara/rails'
 require 'shoulda/matchers'
