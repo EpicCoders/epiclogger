786826996f9fc92bf79233c1665a8b3c2be72343
panioglo <panioglo.srj@gmail.com>
2016-07-05 08:51:00 +0300

    specs for the update method

--- spec/controllers/website_members_controller_spec.rb
+++ spec/controllers/website_members_controller_spec.rb
@@ -18,6 +18,25 @@ RSpec.describe WebsiteMembersController, :type => :controller do
     end
   end
 
+  describe "PUT #update" do
+    let(:params) {{ id: website_member.id, website_member: { realtime: 1, daily_reporting: 1, weekly_reporting: 0, frequent_event: 1 } }}
+
+    it "should update user" do
+      expect{
+        put_with user, :update, params
+        website_member.reload
+      }.to change{ website_member.realtime }.from(false).to(true)
+       .and change{ website_member.daily_reporting }.from(false).to(true)
+       .and change{ website_member.weekly_reporting }.from(true).to(false)
+       .and change{ website_member.frequent_event }.from(false).to(true)
+    end
+
+    it 'should redirect to notifications tab' do
+      expect( put_with user, :update, params ).to redirect_to(redirect_to installations_url(details_tab: 'notifications', main_tab: 'details'))
+      expect( flash[:notice] ).to eq('Successfully updated')
+    end
+  end
+
   describe "DELETE #destroy" do
     let(:user2) { create :user, email: 'user@email.com' }
     let!(:website_member2) { create :website_member, user: user2, website: website }
--- spec/routing/website_members_controller_routing_spec.rb
+++ spec/routing/website_members_controller_routing_spec.rb
@@ -6,11 +6,15 @@ RSpec.describe WebsiteMembersController, type: :routing do
   let(:website_member) { create :website_member, website: website, user: user }
 
   describe 'routing' do
-    it 'routes to #websites/index' do
+    it 'routes to #website_members/index' do
       expect(get('/website_members')).to route_to('website_members#index')
     end
 
-    it 'routes to #webistes/destroy' do
+    it 'routes to #website_members/update' do
+      expect(put("/website_members/#{website_member.id}")).to route_to('website_members#update', id: "#{website_member.id}")
+    end
+
+    it 'routes to #website_members/destroy' do
       expect(delete("/website_members/#{website_member.id}")).to route_to('website_members#destroy', id: "#{website_member.id}")
     end
   end
