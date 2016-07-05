d177d7a9c7ffdd2e7a5661f8ffcb601eeb836cb8
panioglo <panioglo.srj@gmail.com>
2016-07-05 08:21:09 +0300

    fixed and tested notifications system

--- app/controllers/website_members_controller.rb
+++ app/controllers/website_members_controller.rb
@@ -4,6 +4,11 @@ class WebsiteMembersController < ApplicationController
     @website_members = current_website.website_members
   end
 
+  def update
+    @website_member.update_attributes(website_member_params)
+    redirect_to installations_url(details_tab: 'notifications', main_tab: 'details'), notice: 'Successfully updated'
+  end
+
   def destroy
     @website_member.destroy
     if @website_member.errors.full_messages.blank?
@@ -11,5 +16,10 @@ class WebsiteMembersController < ApplicationController
     else
       redirect_to website_members_url, notice: @website_member.errors.full_messages.join(', ')
     end
+  end
+  private
+
+  def website_member_params
+    params.require(:website_member).permit(:realtime, :frequent_event, :daily_reporting, :weekly_reporting)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ Rails.application.routes.draw do
   resources :subscribers, only: [:index, :destroy]
   # resources :accounts, only: [:show]
   resources :installations, only: [:index]
-  resources :website_members, only: [:index, :destroy]
+  resources :website_members, only: [:index, :update, :destroy]
   # resources :settings, only: [:index]
 
   resources :website_wizard
