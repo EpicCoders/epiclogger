367262c4b6e5c8453cb2e112cdd8d2097e701ca4
Razvan Ciocanel <chocksy@gmail.com>
2016-07-15 15:06:05 +0300

    update integration migration to enable hstore extension

--- db/migrate/20160617093238_change_format_in_configurations.rb
+++ db/migrate/20160617093238_change_format_in_configurations.rb
@@ -1,12 +1,14 @@
 class ChangeFormatInConfigurations < ActiveRecord::Migration
   def up
     remove_column :integrations, :configuration
+    execute "CREATE EXTENSION hstore"
     add_column :integrations, :configuration, :hstore
     execute "CREATE INDEX configuration_gin ON integrations USING GIN(configuration)"
   end
 
   def down
     remove_column :integrations, :configuration
+    execute "DROP EXTENSION hstore"
     add_column :integrations, :configuration, :text
     execute "DROP INDEX configuration_data"
   end
