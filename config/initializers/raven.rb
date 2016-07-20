ffb01bb2f7a025be02bf332bc7587a8bc6a03a8d
panioglo <panioglo.srj@gmail.com>
2016-07-20 10:10:00 +0300

    updated raven initializer

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,3 +1,7 @@
-Raven.configure do |config|
-  config.dsn = 'http://jhSvoNawc0T7zHgkA3dDtg:KwLR14sH@localhost:3000/1'
+if ENV['EPICLOGGER_DSN'].present?
+  Raven.configure do |config|
+    config.dsn = ENV['EPICLOGGER_DSN']
+    config.current_environment = Rails.env
+    config.tags = { environment: Rails.env }
+  end
 end
