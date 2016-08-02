20c8dd0624fb9631edc2ef62d9535d37489837dc
panioglo <panioglo.srj@gmail.com>
2016-08-02 10:08:31 +0300

    added a filters tab in the errors sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require turbolinks
 //= require bootstrap
 //= require moment
+//= require bootstrap-datetimepicker
 
 //= require jquery.validate
 //= require jquery.validate.additional-methods
