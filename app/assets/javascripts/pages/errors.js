f7197895a06af0a65ac02db9acb8af462fbe218d
panioglo <panioglo.srj@gmail.com>
2016-08-03 20:33:15 +0300

    required js for daterangepicker to work

--- app/assets/javascripts/pages/errors.js
+++ app/assets/javascripts/pages/errors.js
@@ -1,24 +1,3 @@
-function addKeysToUrl(uri, key, value) {
-  var re = new RegExp("([?&])"+ key +"=[^&#]*", "i");
-  if (re.test(uri)) {
-    uri = uri.replace(re, "$1"+ key +"=" + value);
-  } else {
-    var separator = /\?/.test(uri) ? "&" : "?";
-    uri = uri + separator + key +"=" + value;
-  }
-  return uri
-}
-
-function removeSearchParameter(){
-  uri = window.location.href
-  if (uri.indexOf('search') > -1){
-    value = new RegExp('[\?&]' + 'search' + '=([^&#]*)').exec(uri)[1]
-    keyValue = 'search='+value
-    var re = new RegExp("([&\?]"+ keyValue + "*$|" + keyValue + "&|[?&]" + keyValue + "(?=#))", "i");
-    uri = uri.replace(re, '')
-  }
-  return uri
-}
 $(function () {
   //actions to the sidebar when resolving errors
   var removeFromSidebar = function(e){
@@ -33,33 +12,40 @@ $(function () {
   $('#resolve-button').click(removeFromSidebar);
   $('#unresolve-button').click(removeFromSidebar);
 
-  $('#datetimepicker-from').datetimepicker();
-  $('#datetimepicker-until').datetimepicker({
-    useCurrent: false //Important! See issue #1075
-  });
-  $("#datetimepickesearchr-from").on("dp.change", function (e) {
-    $('#datetimepicker-until').data("DateTimePicker").minDate(e.date);
-  });
-  $("#datetimepicker-until").on("dp.change", function (e) {
-    $('#datetimepicker-from').data("DateTimePicker").maxDate(e.date);
+  var start = moment().subtract(29, 'days');
+  var end = moment();
+
+  function cb(start, end) {
+    $('#datetimepicker').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+  }
+
+  $('#datetimepicker').daterangepicker({
+      autoUpdateInput: false,
+      startDate: start,
+      endDate: end,
+      locale: {
+        cancelLabel: 'Clear'
+      },
+      ranges: {
+         'Today': [moment(), moment()],
+         'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+         'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+         'This Month': [moment().startOf('month'), moment().endOf('month')],
+         'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      }
+  }, cb);
+  $('#datetimepicker').on('apply.daterangepicker', function(ev, picker) {
+    $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
   });
 
-  $( ".sidebar-filters button" ).click(function() {
-    uri = window.location.href
-    if (this.id == 'search-button'){
-      uri = addKeysToUrl(uri, 'search', $('#search-input').val())
-    } else {
-      uri = removeSearchParameter() //removing the search parameter simplifies the controller code
-      if ($('#env-filter').val() != 'all') uri = addKeysToUrl(uri, 'env', $('#env-filter').val())
-      if (!isNaN(Date.parse($('#datetimepicker-from input').val()))) uri = addKeysToUrl(uri, 'from', $('#datetimepicker-from input').val())
-      if (!isNaN(Date.parse($('#datetimepicker-until input').val()))) uri = addKeysToUrl(uri, 'until', $('#datetimepicker-until input').val())
-      if ($("#status-filter input:checked").length > 0) uri = addKeysToUrl(uri, 'status', $("#status-filter input:checked")[0].value)
-    }
-    window.location.href = uri;
+  $('#datetimepicker').on('cancel.daterangepicker', function(ev, picker) {
+    $(this).val('');
   });
 
+  cb(start, end);
 
-  $('#resolve-form').submit(function(){
+  $('.header-resolve').submit(function(){
     var checked;
     checked = $('.list-group input:checked');
     if(checked.length > 0){
