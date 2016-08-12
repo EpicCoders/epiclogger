e0354379f373895929a2e880488a09d2493879cb
panioglo <panioglo.srj@gmail.com>
2016-08-12 18:21:44 +0300

    fixed bug with max-height

--- app/assets/javascripts/components/expand-box.js
+++ app/assets/javascripts/components/expand-box.js
@@ -7,7 +7,7 @@ $(function () {
     $el = $(elem);
     $p  = $el.parent();
     $up = $p.parent();
-    $ps = $up.find("div");
+    $ps = $up.find("div .accordion-group");
 
     // measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
     $ps.each(function() {
